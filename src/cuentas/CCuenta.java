a6193edcc34063a0a8276c09d2a62558f1080851
ogascu <ogascu@gmail.com>
2024-03-18 15:31:52 +0100

    Se agrega el proyecto

---
+++ src/cuentas/CCuenta.java
@@ -0,0 +1,88 @@
+package cuentas;
+/**
+ * Esta clase Sirve para operaciones con las cuentas de un cliente
+ * @author Debian
+ * @version 1.0
+ * 
+ */
+public class CCuenta {
+	private String nombre;
+    private String cuenta;
+    private double saldo;
+    private double tipoInterés;
+
+    public CCuenta()
+    {
+    }
+    /**
+     * @param nom. Titular de la cuenta
+     * @param cue. Numero de cuenta
+     * @param sal. Saldo actual de la cuenta
+     * @param tipo. Tipo de cuenta
+     */
+    public CCuenta(String nom, String cue, double sal, double tipo)
+    {
+        setNombre(nom);
+        setCuenta(cue);
+        setSaldo(sal);
+        setTipoInterés(tipo);
+    }
+
+/**
+ * 
+ * @return. Devuelve el saldo de la cuenta
+ */
+
+    public double estado()
+    {
+        return getSaldo();
+    }
+
+    public void ingresar(double cantidad) throws Exception
+    {
+        if (cantidad<0)
+            throw new Exception("No se puede ingresar una cantidad negativa");
+        setSaldo(getSaldo() + cantidad);
+    }
+
+    public void retirar(double cantidad) throws Exception
+    {
+        if (cantidad <= 0)
+            throw new Exception ("No se puede retirar una cantidad negativa");
+        if (estado()< cantidad)
+            throw new Exception ("No se hay suficiente saldo");
+        setSaldo(getSaldo() - cantidad);
+    }
+
+	private String getNombre() {
+		return nombre;
+	}
+
+	private void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	private String getCuenta() {
+		return cuenta;
+	}
+
+	private void setCuenta(String cuenta) {
+		this.cuenta = cuenta;
+	}
+
+	private double getSaldo() {
+		return saldo;
+	}
+
+	private void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+
+	private double getTipoInterés() {
+		return tipoInterés;
+	}
+
+	private void setTipoInterés(double tipoInterés) {
+		this.tipoInterés = tipoInterés;
+	}
+}
